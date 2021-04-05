ae1bdf299db07641b77e399e6a1df1126eab320a
인선 강 <sunink1@naver.com>
2021-04-05 22:06:27 +0900

    2021-04-05

--- medical/models.py
+++ medical/models.py
@@ -1,15 +1,34 @@
 # --------------------------------- [edit] ---------------------------------- #
-from medical import db
+from medical import db,client
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+# from flask.ext.mongoalchemy  import  MongoAlchemy 
 
 
 
-
-class User(db.Model): #데이터 모델을 나타내는 객체 선언
+class User(db.Model): #usertable
+    __tablename__ = 'usertable'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
     id = db.Column(db.Integer, primary_key=True)
-    nickname= db.Column(db.String(32), unique=True)
-    email = db.Column(db.String(32), unique=True)
+    nickname= db.Column(db.String(64), primary_key=True, unique=True)
+    email = db.Column(db.String(64), unique=True)
     name = db.Column(db.String(32),nullable=False)
-    pw = db.Column(db.String(100),nullable=False)
-    data=db.Column(db.DATE)
+    pw = db.Column(db.String(64),nullable=False)
+    data=db.Column(db.DATE,nullable=False)
     usertype=db.Column(db.String(32))
+
+
+class Post_history(db.Model): #post history table
+    __tablename__ = 'post_history_table'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+    _id2 = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, ForeignKey('usertable.id'))
+    date = db.Column(db.DATE,nullable=False)
+
+
+
+mydb = client['medical'] #db name
+mycol = mydb['community_post'] #collection name
+# community = mycol.insert_one({"id":"", "userid":"", "date":"", "content":"", "comments":"", "like":""})
