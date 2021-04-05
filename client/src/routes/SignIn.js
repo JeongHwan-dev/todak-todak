27b97f439e26fd3224d619165f7c9d21fcd67cd7
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-04-05 05:59:40 +0000

    Add jsconfig.json

--- client/src/routes/SignIn.js
+++ client/src/routes/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
-import './css/SignIn.css';
+import 'routes/css/SignIn.css';
 
 axios.defaults.withCredentials = true;
 
