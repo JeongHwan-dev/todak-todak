c2b07369662255d490367e1b5d04680c5e667b6e
soojin <psych2ros@naver.com>
2021-04-08 13:22:12 +0000

    add new Tables for socket chatting
    - channel table : room for chatting
    - message table : it would be disappear cuz we will use mongoDB later...

--- medical/models.py
+++ medical/models.py
@@ -1,21 +1,44 @@
 from medical import db
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, DateTime, Column, Integer, String
 from sqlalchemy.orm import relationship, backref
-# from flask.ext.mongoalchemy  import  MongoAlchemy 
+# from flask.ext.mongoalchemy  import  MongoAlchemy
 
 
-class User(db.Model): #usertable
+class User(db.Model):  # usertable
     __tablename__ = 'user'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nickname= db.Column(db.String(64), primary_key=True, unique=True)
+    nickname = db.Column(db.String(64), primary_key=True, unique=True)
     email = db.Column(db.String(64), unique=True)
-    name = db.Column(db.String(32),nullable=False)
-    pw = db.Column(db.String(64),nullable=False)
-    date=db.Column(db.DATE,nullable=False)
-    usertype=db.Column(db.String(32))
+    name = db.Column(db.String(32), nullable=False)
+    pw = db.Column(db.String(64), nullable=False)
+    date = db.Column(db.DATE, nullable=False)
+    usertype = db.Column(db.String(32))
 
+ # 넣을 때 항상 정렬 해서 넣기
+
+
+class Channel(db.Model):
+    __tablename__ = 'channel'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(32))
+    user_one = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_two = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.Text)
+    from_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    to_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'))
+    timestamp = db.Column(db.DateTime)
 
 # class Post_history(db.Model): #post history table
 #     tablename = 'post_history_table'
