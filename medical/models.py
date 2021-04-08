81c894f0503c59c3f8bc0c93aeccdaf7e21626ca
soojin <psych2ros@naver.com>
2021-04-08 13:33:23 +0000

    Refactor db model
    deleted 'db.'

--- medical/models.py
+++ medical/models.py
@@ -1,5 +1,5 @@
 from medical import db
-from sqlalchemy import ForeignKey, DateTime, Column, Integer, String
+from sqlalchemy import ForeignKey, DateTime, Column, Integer, String, DATE, Text
 from sqlalchemy.orm import relationship, backref
 # from flask.ext.mongoalchemy  import  MongoAlchemy
 
@@ -8,13 +8,13 @@ class User(db.Model):  # usertable
     __tablename__ = 'user'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nickname = db.Column(db.String(64), primary_key=True, unique=True)
-    email = db.Column(db.String(64), unique=True)
-    name = db.Column(db.String(32), nullable=False)
-    pw = db.Column(db.String(64), nullable=False)
-    date = db.Column(db.DATE, nullable=False)
-    usertype = db.Column(db.String(32))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nickname = Column(String(64), primary_key=True, unique=True)
+    email = Column(String(64), unique=True)
+    name = Column(String(32), nullable=False)
+    pw = Column(String(64), nullable=False)
+    date = Column(DATE, nullable=False)
+    usertype = Column(String(32))
 
  # 넣을 때 항상 정렬 해서 넣기
 
@@ -23,22 +23,22 @@ class Channel(db.Model):
     __tablename__ = 'channel'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(32))
-    user_one = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user_two = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(32))
+    user_one = Column(Integer, ForeignKey('user.id'))
+    user_two = Column(Integer, ForeignKey('user.id'))
 
 
 class Message(db.Model):
     __tablename__ = 'message'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    message = db.Column(db.Text)
-    from_user = db.Column(db.Integer, db.ForeignKey('user.id'))
-    to_user = db.Column(db.Integer, db.ForeignKey('user.id'))
-    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'))
-    timestamp = db.Column(db.DateTime)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    message = Column(Text)
+    from_user = Column(Integer, ForeignKey('user.id'))
+    to_user = Column(Integer, ForeignKey('user.id'))
+    channel_id = Column(Integer, ForeignKey('channel.id'))
+    timestamp = Column(DateTime)
 
 # class Post_history(db.Model): #post history table
 #     tablename = 'post_history_table'
