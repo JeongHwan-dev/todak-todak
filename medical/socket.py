a8599938268b296a2105ab784c8fb375552d0014
soojin <psych2ros@naver.com>
2021-04-09 08:27:03 +0000

    add chat
    - need to merge

--- medical/socket.py
+++ medical/socket.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, Blueprint
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
 socketio = SocketIO(cors_allowed_origins="*")
@@ -13,20 +13,19 @@ def handle_message(message):
 
 
 @socketio.on('join')
-def on_join(data):
-    print("haha!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print("================================\n",
-          data, "===============================\n")
-    username = data['username']
-    room = data['room']
+def on_join(channel_data):
+    # print("================================\n",
+    #       data, "===============================\n")
+    username = channel_data['username']
+    room = channel_data['room']
     join_room(room)
     send(username + ' has entered the room.', room=room)
 
 
 @socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
+def on_leave(channel_data):
+    user_one = channel_data['username']
+    room = channel_data['room']
     leave_room(room)
     send(username + ' has left the room.', room=room)
 
