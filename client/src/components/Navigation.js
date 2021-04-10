d7cdb3ae4bb01e452927b96f625dbe7ce6fe19de
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-04-10 13:40:34 +0900

    Fix route

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import 'components/css/Navigation.css';
 
 // 네비게이션 컴포넌트
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Community from 'components/Community';
-import Navigation from 'components/Navigation';
 
 // 홈 페이지
 const Home = () => {
   return (
     <div>
       <div className="home-title">
-        <h1>Home Page</h1>
+        <h1>토닥토닥</h1>
         <hr />
-      </div>
-      <div>
-        <Navigation />
       </div>
       <div className="community-container">
         <Community />
